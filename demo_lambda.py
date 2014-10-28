46bd1b90627224af1241d3d4f0bc5d7328b8128d
David Owen <david.owen.14@ucl.ac.uk>
2014-10-28 19:40:15 +0000

    Added a new demo and cleared up old demo.

--- demo_lambda.py
+++ demo_lambda.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python
 from mc_diffusion_step import *
-from default_energy import *
 from matplotlib import pyplot as plt
 from time import sleep
 
-""" examples of using the Monte Carlo diffusion simulator """ 
+""" example of using the Monte Carlo diffusion simulator """ 
+
+cube = lambda x : x**3
+func_energy = lambda x : sum(map(cube, x))
+
 density = [500, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 
 plt.plot(density)
 plt.show(block=False)
 # using the default energy function
 for i in range(1, 100000):
-    density = diff_step(default_energy, density, 100)
+    density = diff_step(func_energy, density, 100)
     if i % 1000 == 0:
         plt.plot(density)
         sleep(0.05)
